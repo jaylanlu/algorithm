968793beea537e3affe24bd819c31012f8f570a9
jaylanlu <1319213798@qq.com>
2019-01-19 13:18:15 +0800

    sss

---
+++ learn_test/Block/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Block
+//
+//  Created by Jaylan on 2018/11/27.
+//  Copyright © 2018 Jaylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learn_test/Block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Block
+//
+//  Created by Jaylan on 2018/11/27.
+//  Copyright © 2018 Jaylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
