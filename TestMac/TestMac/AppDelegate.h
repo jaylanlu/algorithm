d467a344658bdbb4b4e641ff7139002f63454897
Jaylan <jaylan.lu@corp.to8to.com>
2019-04-28 14:17:57 +0800

    ...

---
+++ TestMac/TestMac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMac
+//
+//  Created by Jaylan on 2019/4/26.
+//  Copyright © 2019 Jaylan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
