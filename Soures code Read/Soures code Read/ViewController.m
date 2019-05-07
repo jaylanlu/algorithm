7bf370f55fbac1dcefb8440d653f02bbe5dc7670
Jaylan <jaylan.lu@corp.to8to.com>
2019-05-07 11:00:27 +0800

    sfadfsa

--- Soures code Read/Soures code Read/ViewController.m
+++ Soures code Read/Soures code Read/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+typedef void(^Task)(BOOL);
+typedef void(^Blk)(void);
 @interface ViewController ()
 
 @end
@@ -16,7 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    Task task = [self delay:10 task:^{
+        NSLog(@"sfas");
+    }];
+    task(false);
+}
+
+- (Task)delay:(NSTimeInterval)time task:(Blk)task {
+    __block Task result;
+    __block Blk closure = task;
+    Task delayBlock = ^(BOOL cancel){
+        if (closure) {
+            Blk internalBlock = closure;
+            if (!cancel) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    internalBlock();
+                });
+            }
+            closure = nil;
+        }
+        result = nil;
+    };
+    result = delayBlock;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (result) {
+            result(false);
+        }
+    });
+    return result;
+    
 }
 
 
